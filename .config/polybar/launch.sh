c978a2b94c0f19ca870ce19fe7280dfa7a91c85c
llGaetanll <gaetan.almela@gmail.com>
2020-09-06 16:29:55 -0400

    updated polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,4 +9,4 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar
-polybar -c ~/.config/polybar/config.ini main &
+polybar -c ~/.config/polybar/config bottom &
