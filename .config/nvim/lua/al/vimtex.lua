465c974304cbaf2d3dec4a2c81a9d4e5c5a6f21a
Gaetan Almela <gaetan.almela@gmail.com>
2023-02-21 23:45:35 -0500

    vimtex setup cleanup

--- .config/nvim/lua/al/vimtex.lua
+++ .config/nvim/lua/al/vimtex.lua
@@ -1,11 +1,10 @@
 local status_ok, _ = pcall(require, "vimtex")
--- if not status_ok then
---   return
--- end
+if not status_ok then
+  return
+end
+
+vim.cmd [[ syntax enable ]]
 
 -- Error Suppression
 -- https://github.com/lervag/vimtex/blob/master/doc/vimtex.txt
-vim.g.vimtex_log_ignore = {
-  'Underfull',
-  'Overfull',
-}
+vim.g.vimtex_log_ignore = { "Underfull", "Overfull" }
