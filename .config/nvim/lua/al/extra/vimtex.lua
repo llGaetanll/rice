a238495b268e44637ebb52697e024c9afeff9e9e
Gaetan Almela <gaetan.almela@gmail.com>
2023-05-27 17:07:14 -0400

    vimtex now checks for zathura to load

--- .config/nvim/lua/al/extra/vimtex.lua
+++ .config/nvim/lua/al/extra/vimtex.lua
@@ -8,3 +8,4 @@ vim.cmd [[ syntax enable ]]
 -- Error Suppression
 -- https://github.com/lervag/vimtex/blob/master/doc/vimtex.txt
 vim.g.vimtex_log_ignore = { "Underfull", "Overfull" }
+vim.g.vimtex_view_method = 'zathura'
