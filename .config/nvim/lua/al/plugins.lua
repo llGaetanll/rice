c443de90e34d8e144dccc34030c4a9cbe8ce0337
Gaetan Almela <gaetan.almela@gmail.com>
2023-05-27 17:10:05 -0400

    reformated file

--- .config/nvim/lua/al/plugins.lua
+++ .config/nvim/lua/al/plugins.lua
@@ -40,115 +40,106 @@ packer.init({
 
 -- Install your plugins here
 return packer.startup(function(use)
-  -- Have packer manage itself
-  -- 	this is the package manager
-  use "wbthomason/packer.nvim"
-
-
+	-- Have packer manage itself
+	-- 	this is the package manager
+	use("wbthomason/packer.nvim")
 
 	-- Colorschemes
-  -- for more TreeSitter colorschemes
-  -- see: https://github.com/rockerBOO/awesome-neovim#tree-sitter-supported-colorscheme
-  use "savq/melange"
-  use "mhartington/oceanic-next"
-  use "kyazdani42/blue-moon"
-  use "sainnhe/everforest"
-  use "martinsione/darkplus.nvim"
-  use "ellisonleao/gruvbox.nvim"
+	-- for more TreeSitter colorschemes
+	-- see: https://github.com/rockerBOO/awesome-neovim#tree-sitter-supported-colorscheme
+	use("savq/melange")
+	use("mhartington/oceanic-next")
+	use("kyazdani42/blue-moon")
+	use("sainnhe/everforest")
+	use("martinsione/darkplus.nvim")
+	use("ellisonleao/gruvbox.nvim")
 
+	-- TreeSitter
+	-- parses the file much more accurately to provide better commenting / syntax-highlighting
+	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	use("p00f/nvim-ts-rainbow") -- color match parentheses
+	use("nvim-treesitter/playground")
 
+	-- Commenting
+	use({
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	})
+	use("JoosepAlviste/nvim-ts-context-commentstring") -- for native jsx context-aware commenting
 
-  -- TreeSitter
-  -- parses the file much more accurately to provide better commenting / syntax-highlighting
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use "p00f/nvim-ts-rainbow"                        -- color match parentheses
-  use 'nvim-treesitter/playground'
+	use("lukas-reineke/indent-blankline.nvim") -- display indents
 
-  -- Commenting
-  use {
-	  'numToStr/Comment.nvim',
-    config = function()
-      require('Comment').setup()
-    end
-  }
-  use 'JoosepAlviste/nvim-ts-context-commentstring' -- for native jsx context-aware commenting
+	-- CMP - Autocomplete
+	-- This is actually necessary for lsp to work
+	use("hrsh7th/nvim-cmp") -- base
+	use("hrsh7th/cmp-nvim-lsp") -- autocompletion from nvim lsp
+	use("hrsh7th/cmp-buffer") -- autocompletion from buffers
+	use("hrsh7th/cmp-path") -- autocompletion for paths
+	use("hrsh7th/cmp-cmdline") -- autocompletion for nvim commands
 
-  use "lukas-reineke/indent-blankline.nvim"         -- display indents
+	-- snippets
+	use("L3MON4D3/LuaSnip") -- snippets used in autocompletion
+	use("saadparwaiz1/cmp_luasnip") -- luasnip completion source for nvim-cmp
+	use("rafamadriz/friendly-snippets") -- massive repo of popular snippets for autocompletion
 
+	-- autopairs
+	use("windwp/nvim-autopairs") -- autocomplete parentheses, brackets, etc...
 
+	-- file formatting
+	use("mhartington/formatter.nvim") -- automatically format files on save
 
-  -- CMP - Autocomplete
-  -- This is actually necessary for lsp to work
-  use 'hrsh7th/nvim-cmp'                            -- base
-  use "hrsh7th/cmp-nvim-lsp"                        -- autocompletion from nvim lsp
-  use 'hrsh7th/cmp-buffer'                          -- autocompletion from buffers
-  use 'hrsh7th/cmp-path'                            -- autocompletion for paths
-  use 'hrsh7th/cmp-cmdline'                         -- autocompletion for nvim commands
+	-- Telescope fuzzy-finding
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.1",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
-  -- snippets
-  use "L3MON4D3/LuaSnip"                            -- snippets used in autocompletion
-  use "saadparwaiz1/cmp_luasnip"                    -- luasnip completion source for nvim-cmp
-  use "rafamadriz/friendly-snippets"                -- massive repo of popular snippets for autocompletion
+	-- Telescope for Snippets
+	use({
+		"benfowler/telescope-luasnip.nvim",
+		module = "telescope._extensions.luasnip", -- if you wish to lazy-load
+	})
 
-  -- autopairs
-  use "windwp/nvim-autopairs"                       -- autocomplete parentheses, brackets, etc...
+	--- LSP - Language Server Protocol
+	use({
+		"williamboman/mason.nvim", -- simple to use language server installer
+		"williamboman/mason-lspconfig.nvim", -- allows interop between mason and lsconfig
+		"neovim/nvim-lspconfig", -- enable lsp
+	})
 
-  -- file formatting
-  use "mhartington/formatter.nvim"                  -- automatically format files on save
+	-- GPT autocompletion
+	use("github/copilot.vim") -- github copilot in nvim
 
-  -- Telescope fuzzy-finding
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
+	-- OTHER
+	use("folke/which-key.nvim") -- tells you all your keybinds
 
-  -- Telescope for Snippets
-  use {
-      "benfowler/telescope-luasnip.nvim",
-      module = "telescope._extensions.luasnip",  -- if you wish to lazy-load
-  }
+	use("akinsho/bufferline.nvim") -- pseudo "tabs" to work with nvim tree
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	}) -- airline for lua
+	use("kyazdani42/nvim-tree.lua") -- filesystem tree
+	use("kyazdani42/nvim-web-devicons") -- icons for nvim tree
+	use("lewis6991/gitsigns.nvim") -- git indicators
+	use("norcalli/nvim-colorizer.lua") -- colorize CSS color codes
 
+	-- language specific plugins
+	use("lervag/vimtex") -- latex support
 
-  --- LSP - Language Server Protocol
-  use {
-    'williamboman/mason.nvim',                      -- simple to use language server installer
-    'williamboman/mason-lspconfig.nvim',            -- allows interop between mason and lsconfig
-    'neovim/nvim-lspconfig'                         -- enable lsp
-  }
+	-- markdown preview
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	})
 
-  -- GPT autocompletion
-  use 'github/copilot.vim'                          -- github copilot in nvim
-
-
-
-  -- OTHER
-  use "folke/which-key.nvim"                        -- tells you all your keybinds
-
-  use "akinsho/bufferline.nvim"                     -- pseudo "tabs" to work with nvim tree
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }                                                 -- airline for lua
-  use "kyazdani42/nvim-tree.lua"                    -- filesystem tree 
-  use "kyazdani42/nvim-web-devicons"                -- icons for nvim tree
-  use "lewis6991/gitsigns.nvim"                     -- git indicators
-  use "norcalli/nvim-colorizer.lua"                 -- colorize CSS color codes
-
-
-
-  -- language specific plugins
-  use 'lervag/vimtex'                               -- latex support
-
-  -- markdown preview
-  use({
-    "iamcco/markdown-preview.nvim",
-    run = function() vim.fn["mkdp#util#install"]() end,
-  })
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-	  require("packer").sync()
-  end
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
 end)
-
