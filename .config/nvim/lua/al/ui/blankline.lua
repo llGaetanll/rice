97f825d142556e70509693b7a5aa7d3b151926e1
Gaetan Almela <gaetan.almela@gmail.com>
2024-05-22 11:14:34 -0400

    disabled blankline
    generally slows down nvim

--- .config/nvim/lua/al/ui/blankline.lua
+++ .config/nvim/lua/al/ui/blankline.lua
@@ -1,12 +1,12 @@
-local status_ok, blankline = pcall(require, "ibl")
-if not status_ok then
-	return
-end
+-- local status_ok, blankline = pcall(require, "ibl")
+-- if not status_ok then
+-- 	return
+-- end
 
-vim.opt.list = true
--- vim.opt.listchars:append "space:⋅"
-vim.opt.listchars:append("eol:↴")
+-- vim.opt.list = true
+-- vim.opt.listchars:append("space:⋅")
+-- vim.opt.listchars:append("eol:↴")
 
 -- blankline.setup({
---     scope = { enabled = false },
+-- 	scope = { enabled = false },
 -- })
