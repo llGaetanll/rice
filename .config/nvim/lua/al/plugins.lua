f63ecb89b7e1f04756ba09d4221c4b6fab13f8d1
Gaetan Almela <gaetan.almela@gmail.com>
2024-08-11 13:46:13 -0400

    move treesitter config to plugins
    see: https://www.lazyvim.org/plugins/treesitter

--- .config/nvim/lua/al/plugins.lua
+++ .config/nvim/lua/al/plugins.lua
@@ -36,7 +36,43 @@ lazy.setup({
 
     --[[ TreeSitter ]]
     -- parses the file much more accurately to provide better commenting / syntax-highlighting
-    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    {
+      "nvim-treesitter/nvim-treesitter",
+      build = ":TSUpdate",
+      event = { "VeryLazy" },
+      lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+      init = function(plugin)
+        -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+        -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+        -- no longer trigger the **nvim-treesitter** module to be loaded in time.
+        -- Luckily, the only things that those plugins need are the custom queries, which we make available
+        -- during startup.
+        require("lazy.core.loader").add_to_rtp(plugin)
+        require("nvim-treesitter.query_predicates")
+      end,
+      cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+      opts = {
+        ensure_installed = { "rust" }, -- one of "all" or a list of languages
+        ignore_install = { "" }, -- List of parsers to ignore installing
+        highlight = {
+          enable = true, -- false will disable the whole extension
+          disable = {
+            "css",
+            "latex",
+          }, -- list of language that will be disabled
+        },
+        autopairs = {
+          enable = true,
+        },
+        indent = {
+          enable = true,
+          disable = { "python", "css" },
+        },
+      },
+      config = function(_, opts)
+        require("nvim-treesitter.configs").setup(opts)
+      end,
+    },
     "hiphish/rainbow-delimiters.nvim",
     "nvim-treesitter/playground",
 
