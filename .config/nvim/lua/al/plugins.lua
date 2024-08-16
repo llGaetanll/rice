6eba089198d9394e815e69f891c6494a4bf50d13
Gaetan Almela <gaetan.almela@gmail.com>
2024-08-16 12:14:01 -0400

    telescope version bump

--- .config/nvim/lua/al/plugins.lua
+++ .config/nvim/lua/al/plugins.lua
@@ -118,7 +118,7 @@ lazy.setup({
     -- fuzzy-finding
     {
       "nvim-telescope/telescope.nvim",
-      tag = "0.1.4",
+      tag = "0.1.8",
       dependencies = { "nvim-lua/plenary.nvim" },
       opts = require("al.core.search")
     },
