7a67e77ed978663d832832e4f3fedf1c09a98823
Gaetan Almela <gaetan.almela@gmail.com>
2024-08-18 11:49:24 -0400

    rust_analyzer no longer merges imports by default

--- .config/nvim/lua/al/core/completion/servers/rust_analyzer.lua
+++ .config/nvim/lua/al/core/completion/servers/rust_analyzer.lua
@@ -7,6 +7,9 @@ return {
             diagnostics = {
                 enable = true,
             },
+            imports = {
+                granularity = { group = "item" },
+            },
             inlayHints = {
                 parameterHints = true,
                 bindingModeHints = {
