51238efe6a9181b96453df990ed718fcd5892352
Gaetan Almela <gaetan@ranskytech.com>
2025-05-12 17:56:35 -0400

    Remove snippets smarttab
    Was way too annoying, hijacked `<Tab>` constantly

--- .config/nvim/lua/al/core/completion/cmp.lua
+++ .config/nvim/lua/al/core/completion/cmp.lua
@@ -61,10 +61,6 @@ cmp.setup {
         ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
-            elseif luasnip.expandable() then
-                luasnip.expand()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
             else
                 fallback()
             end
@@ -75,8 +71,6 @@ cmp.setup {
         ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
-            elseif luasnip.locally_jumpable(-1) then
-                luasnip.jump(-1)
             else
                 fallback()
             end
