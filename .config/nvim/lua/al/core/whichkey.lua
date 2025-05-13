df8ffe3d749ea07d621af546cd4fcabc48439858
Gaetan Almela <gaetan@ranskytech.com>
2025-05-13 10:34:46 -0400

    Format noise

--- .config/nvim/lua/al/core/whichkey.lua
+++ .config/nvim/lua/al/core/whichkey.lua
@@ -1,31 +1,31 @@
 -- whichkey config params
 return {
     plugins = {
-        marks = true, -- shows a list of your marks on ' and `
-        registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+        marks = true,         -- shows a list of your marks on ' and `
+        registers = true,     -- shows your registers on " in NORMAL or <C-r> in INSERT mode
         spelling = {
-            enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+            enabled = true,   -- enabling this will show WhichKey when pressing z= to select spelling suggestions
             suggestions = 20, -- how many suggestions should be shown in the list?
         },
 
         -- the presets plugin, adds help for a bunch of default keybindings in Neovim
         -- No actual key bindings are created
         presets = {
-            operators = true, -- adds help for operators like d, y, ... and registers them for motion / text object completion
-            motions = true, -- adds help for motions
+            operators = true,    -- adds help for operators like d, y, ... and registers them for motion / text object completion
+            motions = true,      -- adds help for motions
             text_objects = true, -- help for text objects triggered after entering an operator
-            windows = true, -- default bindings on <c-w>
-            nav = true, -- misc bindings to work with windows
-            z = true, -- bindings for folds, spelling and others prefixed with z
-            g = true, -- bindings for prefixed with g
+            windows = true,      -- default bindings on <c-w>
+            nav = true,          -- misc bindings to work with windows
+            z = true,            -- bindings for folds, spelling and others prefixed with z
+            g = true,            -- bindings for prefixed with g
         },
     },
 
     icons = {
         breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-        separator = " ", -- symbol used between a key and it's label
-        group = "+", -- symbol prepended to a group
-        rules = false, -- disable keymap icons
+        separator = " ",  -- symbol used between a key and it's label
+        group = "+",      -- symbol prepended to a group
+        rules = false,    -- disable keymap icons
         keys = {
             Up = " ",
             Down = " ",
@@ -60,11 +60,11 @@ return {
 
     keys = {
         scroll_down = "<c-d>", -- binding to scroll down inside the popup
-        scroll_up = "<c-u>", -- binding to scroll up inside the popup
+        scroll_up = "<c-u>",   -- binding to scroll up inside the popup
     },
 
     win = {
-        border = "none", -- none, single, double, shadow
+        border = "none",          -- none, single, double, shadow
         padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
         wo = {
             winblend = 0,
@@ -74,8 +74,8 @@ return {
     layout = {
         height = { min = 4, max = 25 }, -- min and max height of the columns
         width = { min = 20, max = 50 }, -- min and max width of the columns
-        spacing = 3, -- spacing between columns
-        align = "center", -- align columns left, center or right
+        spacing = 3,                    -- spacing between columns
+        align = "center",               -- align columns left, center or right
     },
 
     show_help = true, -- show help message on the command line when the popup is visible
