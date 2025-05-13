253e63d16775f8ad0b4d70421e1c9ee2177f66c6
Gaetan Almela <gaetan@ranskytech.com>
2025-05-13 06:08:43 -0400

    Cleanup LSP config

--- .config/nvim/lua/al/core/completion/lsp.lua
+++ .config/nvim/lua/al/core/completion/lsp.lua
@@ -52,159 +52,92 @@ local lsp_references = function()
     require("telescope.builtin").lsp_references()
 end
 
+local hover = function()
+    -- Extends `vim.lsp.util.open_floating_preview.Opts`
+    vim.lsp.buf.hover {
+        border = "rounded",
+        max_width = 50,
+    }
+end
+
 local lsp_keymaps = {
-    -- goto definition of variable
-    {
-        mode = "n",
-        keymap = "gd",
-        action = vim.lsp.buf.definition,
-        desc = "[G]et [d]efinition",
-    },
+    { mode = "n", keymap = "gd",         action = vim.lsp.buf.definition,     desc = "[g]oto [d]efinition", },
+    { mode = "n", keymap = "K",          action = hover,                      desc = "Get object Info", },
+    { mode = "n", keymap = "gS",         action = vim.lsp.buf.signature_help, desc = "[g]et [S]ignature", },
+    { mode = "n", keymap = "gi",         action = lsp_implementations,        desc = "[g]et [i]mplementation", },
+    { mode = "n", keymap = "<leader>rn", action = vim.lsp.buf.rename,         desc = "[r]e[n]ame" },
+    { mode = "n", keymap = "gr",         action = lsp_references,             desc = "[g]et [r]eferences", },
+    { mode = "n", keymap = "<leader>ca", action = vim.lsp.buf.code_action,    desc = "[c]ode [a]ctions", },
+    { mode = "n", keymap = "<leader>f",  action = vim.diagnostic.open_float,  desc = "Display info about errors", },
+    { mode = "n", keymap = "<leader>q",  action = vim.diagnostic.setloclist,  desc = "Set Loc List", },
 
-    -- get info about object
-    {
-        mode = "n",
-        keymap = "K",
-        action = function()
-            -- This config extends `vim.lsp.util.open_floating_preview.Opts`
-            vim.lsp.buf.hover {
-                border = "rounded",
-                max_width = 50,
-            }
-        end,
-        desc = "Get Info",
-    },
+    { mode = "n", keymap = "[d",         action = vim.diagnostic.goto_prev,   desc = "Prev Def" },
+    { mode = "n", keymap = "]d",         action = vim.diagnostic.goto_next,   desc = "Next Def" },
 
-    -- get signature of fn
-    {
-        mode = "n",
-        keymap = "gS",
-        action = vim.lsp.buf.signature_help,
-        desc = "[G]et [S]ignature",
-    },
+    { mode = "n", keymap = "[e",         action = lsp_gotos.prev_error,       desc = "Prev Error" },
+    { mode = "n", keymap = "]e",         action = lsp_gotos.next_error,       desc = "Next Error" },
 
-    -- get implementation info
-    {
-        mode = "n",
-        keymap = "gi",
-        action = lsp_implementations,
-        desc = "[G]et [i]mplementation",
-    },
+    { mode = "n", keymap = "[w",         action = lsp_gotos.prev_warn,        desc = "Prev Warning", },
+    { mode = "n", keymap = "]w",         action = lsp_gotos.next_warn,        desc = "Next Warning", },
 
-    -- rename an object
-    { mode = "n", keymap = "<leader>rn", action = vim.lsp.buf.rename, desc = "[r]e[n]ame" },
+    { mode = "n", keymap = "[i",         action = lsp_gotos.prev_info,        desc = "Prev Info" },
+    { mode = "n", keymap = "]i",         action = lsp_gotos.next_info,        desc = "Next Info" },
 
-    -- list all references of object
-    {
-        mode = "n",
-        keymap = "gr",
-        action = lsp_references,
-        desc = "[g]et [r]eferences",
-    },
-
-    -- get code actions
-    {
-        mode = "n",
-        keymap = "<leader>ca",
-        action = vim.lsp.buf.code_action,
-        desc = "[C]ode [A]ctions",
-    },
-
-    -- display info about errors
-    {
-        mode = "n",
-        keymap = "<leader>f",
-        action = vim.diagnostic.open_float,
-        desc = "Display info about errors",
-    },
-
-    -- goto previous def
-    { mode = "n", keymap = "[d", action = vim.diagnostic.goto_prev, desc = "Prev Def" },
-
-    -- goto next def
-    { mode = "n", keymap = "]d", action = vim.diagnostic.goto_next, desc = "Next Def" },
-
-    -- goto previous error
-    { mode = "n", keymap = "[e", action = lsp_gotos.prev_error, desc = "Prev Error" },
-
-    -- goto next error
-    { mode = "n", keymap = "]e", action = lsp_gotos.next_error, desc = "Next Error" },
-
-    -- goto previous warning
-    { mode = "n", keymap = "[w", action = lsp_gotos.prev_warn, desc = "Prev Warning" },
-
-    -- goto next warning
-    { mode = "n", keymap = "]w", action = lsp_gotos.next_warn, desc = "Next Warning" },
-
-    -- goto previous info
-    { mode = "n", keymap = "[i", action = lsp_gotos.prev_info, desc = "Prev Info" },
-
-    -- goto next info
-    { mode = "n", keymap = "]i", action = lsp_gotos.next_info, desc = "Next Info" },
-
-    -- goto previous hint
-    { mode = "n", keymap = "[h", action = lsp_gotos.prev_hint, desc = "Prev Hint" },
-
-    -- goto next error
-    { mode = "n", keymap = "]h", action = lsp_gotos.next_hint, desc = "Next Hint" },
-
-    -- set loc list
-    {
-        mode = "n",
-        keymap = "<leader>q",
-        action = "<cmd>lua vim.diagnostic.setloclist()<CR>",
-        desc = "Set Loc List",
-    },
+    { mode = "n", keymap = "[h",         action = lsp_gotos.prev_hint,        desc = "Prev Hint" },
+    { mode = "n", keymap = "]h",         action = lsp_gotos.next_hint,        desc = "Next Hint" },
 }
-
--- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
-
--- settings directory for language server parameters
-local servers_dir = "al.core.completion.servers"
-
-vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(event)
-        -- LSP key bindings
-        for _, km in ipairs(lsp_keymaps) do
-            vim.keymap.set(
-                km.mode,
-                km.keymap,
-                km.action,
-                { buffer = event.buf, noremap = true, silent = true, desc = "[LSP]: " .. km.desc }
-            )
-        end
-
-        -- Format on save
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = event.buf,
-            callback = function()
-                vim.lsp.buf.format()
-            end,
-        })
-    end,
-})
 
 mason.setup {}
 mason_lsp.setup {}
+
+local function on_attach(client, bufnr)
+    -- LSP key bindings
+    for _, km in ipairs(lsp_keymaps) do
+        vim.keymap.set(
+            km.mode,
+            km.keymap,
+            km.action,
+            { buffer = bufnr, noremap = true, silent = true, desc = "[LSP]: " .. km.desc }
+        )
+    end
+
+    -- Format on save
+    vim.api.nvim_create_autocmd("BufWritePre", {
+        buffer = bufnr,
+        callback = function()
+            vim.lsp.buf.format {
+                -- Formatting for {j/t}sx? files should be handled by prettier
+                filter = function(client)
+                    return client.name ~= "ts_ls"
+                end,
+            }
+        end,
+    })
+end
 
 -- TODO:
 -- 1. We can probably do better than a hardcoded list of servers
 -- 2. Loading the settings upfront for each server like this is wasteful. It
 --    would be better if they were lazily loaded (i.e. when we open a file of the
 --    right type) but for the life of me, I cannot figure out how to do this.
-local servers = { "rust_analyzer", "lua_ls", "hls" }
+local servers = { "rust_analyzer", "lua_ls" }
+
+-- settings directory for language server parameters
+local servers_dir = "al.core.completion.servers"
+
+-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
+
 for _, server in ipairs(servers) do
     local server_dir = servers_dir .. "." .. server
     local conf_ok, conf = pcall(require, server_dir)
-    if conf_ok then
-        vim.lsp.config(server, {
-            capabilities = capabilities,
-            settings = conf.settings,
-            filetypes = conf.filetypes,
-        })
-    end
+    vim.lsp.config(server, {
+        capabilities = capabilities,
+        on_attach = on_attach,
+        settings = conf_ok and conf.settings or nil,
+        filetypes = conf_ok and conf.filetypes or nil,
+    })
 end
 
 -- setup the diagnostics config
